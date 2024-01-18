3318e939fbc2d0884f3c282e51e4ff8f8446749f
Chia-I Wu <olvaffe@gmail.com>
2024-01-17 21:51:43 -0800

    a

--- sh-aliases.sh
+++ sh-aliases.sh
@@ -7,14 +7,18 @@ alias grep='grep --color=auto'
 alias g='grep'
 alias l='less'
 
-alias vi=vim
-alias sh=bash
-
 # source control
 alias gg='git grep --color'
 alias sg='grep --exclude-dir=.svn --exclude-dir=.git -R'
 
 alias fixssh='eval $(tmux showenv -s SSH_AUTH_SOCK)'
+
+if command -v vim > /dev/null; then
+	alias vi='vim'
+fi
+if command -v bash > /dev/null; then
+	alias sh='bash'
+fi
 
 # debian
 if command -v apt > /dev/null; then
