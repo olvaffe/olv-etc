9230c091f07bb41cb3ce700006771dd4d8732c95
Chia-I Wu <olvaffe@gmail.com>
2024-02-06 12:59:41 -0800

    a

--- sh-rc.sh
+++ sh-rc.sh
@@ -9,7 +9,9 @@ esac
 [ -f "$HOME/.config/sh/paths.sh" ] && . "$HOME/.config/sh/paths.sh"
 [ -f "$HOME/.config/sh/aliases.sh" ] && . "$HOME/.config/sh/aliases.sh"
 
-[ -z "$WAYLAND_DISPLAY$DISPLAY" -a $(tty) = "/dev/tty1" ] && exec sway-session
+[ -z "$WAYLAND_DISPLAY$DISPLAY" -a $(tty) = "/dev/tty1" ] && \
+	command -v sway > /dev/null && \
+	exec sway-session
 
 HISTFILE="$HOME/.local/state/sh.history"
 HISTSIZE=64000
